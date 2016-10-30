5dfab519ee86ac37cb455bd8a14be1f8ce62b00d
Tuuux <tuxa@rtnp.org>
2016-10-30 10:50:54 +0100

    Pfffffff

--- GLXCurses/VBox.py
+++ GLXCurses/VBox.py
@@ -3,6 +3,7 @@
 import curses
 from Widget import Widget
 from Box import Box
+
 # It script it publish under GNU GENERAL PUBLIC LICENSE
 # http://www.gnu.org/licenses/gpl-3.0.en.html
 # Author: Jérôme ORNECH alias "Tuux" <tuxa@rtnp.org> all rights reserved
@@ -42,39 +43,40 @@ class VBox(Widget):
 
         # Check widgets to display
         if (widget_height >= self.number_of_widget_to_display + 1) and (
-            widget_width >= self.number_of_widget_to_display + 1):
+                    widget_width >= self.number_of_widget_to_display + 1):
             if len(self.widget_to_display.keys()):
                 devised_box_size = widget_height / self.number_of_widget_to_display
 
-                for count in range(0, self.number_of_widget_to_display, 1):
-                    if count == 0:
-                        self.widget_subwins[count] = self.widget.subwin(
+                for ID in range(0, self.number_of_widget_to_display, 1):
+                    if ID == 0:
+                        self.widget_subwins[ID] = self.widget.subwin(
                             devised_box_size - self.subwins_spacing,
                             widget_width - self.subwins_spacing * 2,
                             widget_y + self.subwins_spacing,
                             widget_x + self.subwins_spacing
                         )
                     else:
-                        self.widget_subwins[count] = self.widget.subwin(
-                            devised_box_size - (self.subwins_spacing/2),
+                        self.widget_subwins[ID] = self.widget.subwin(
+                            devised_box_size - (self.subwins_spacing / 2),
                             widget_width - self.subwins_spacing * 2,
-                            widget_y + (devised_box_size * count) + (self.subwins_spacing/2),
+                            widget_y + (devised_box_size * ID) + (self.subwins_spacing / 2),
                             widget_x + self.subwins_spacing
                         )
 
-                    self.widget_subwins[count].bkgdset(ord(' '), curses.color_pair(10 + count))
-                    self.widget_subwins[count].bkgd(ord(' '), curses.color_pair(10 + count))
+                    self.widget_subwins[ID].bkgdset(ord(' '), curses.color_pair(10 + ID))
+                    self.widget_subwins[ID].bkgd(ord(' '), curses.color_pair(10 + ID))
 
                     # Check widgets to display
-                    #self.h_widget_list[count] = Box(self)
-                    #self.widget_to_display[count].add_parent(self.h_widget_list[count])
-                    #self.h_widget_list[count].set_parent(self.widget_subwins[count])
-                            #self.h_widget_list[count].add(self.h_widget_list[count])
-                            #self.h_widget_list[count].draw()
-                        #self.widget_to_display[0].add
-                        #self.widget_to_display[0].set_parent(self.widget_subwins[count])
-                    #self.widget_to_display[count].draw()
-
+                    # self.h_widget_list[ID] = Box(self)
+                    # self.widget_to_display[ID].set_parent(self.h_widget_list[ID])
+                    # self.h_widget_list[ID].widget = self.widget_subwins[ID]
+                    # self.widget_to_display[ID].add_parent(self.h_widget_list[ID])
+                    # self.h_widget_list[ID].set_parent(self.widget_subwins[ID])
+                    # self.h_widget_list[ID].add(self.h_widget_list[ID])
+                    # self.h_widget_list[ID].draw()
+                    # self.widget_to_display[0].add
+                    # self.widget_to_display[0].set_parent(self.widget_subwins[ID])
+                    # self.widget_to_display[ID].draw()
 
     def set_title(self, title):
         self.title = title
@@ -86,7 +88,3 @@ class VBox(Widget):
         else:
             self.widget_to_display[id_max + 1] = widget
         self.number_of_widget_to_display += 1
-
-
-
-
