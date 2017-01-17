f606c56f485b8ddbe4425642705c771ada00d2cd
Tuuux <tuxa@rtnp.org>
2017-01-17 01:06:12 +0100

    Start Binding thing

---
+++ GLXCurses/Binding.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# It script it publish under GNU GENERAL PUBLIC LICENSE
+# http://www.gnu.org/licenses/gpl-3.0.en.html
+# Author: Jérôme ORNECH alias "Tuux" <tuxa@rtnp.org> all rights reserved
+__author__ = 'Tuux'
+
+
+class Binding(object):
+    """
+    **Bindings**
+
+    Bindings — Key bindings for individual widgets
+    """
+    def __init__(self):
+        self.binding_set = None  # a GtkBindingSet to add a signal to
+        self.keyval = None       # key value
+        self.modifiers = None    # key modifier
+        self.signal_name = None  # signale name to be bound
+        self.binding_args = None # lis of BindingArg signal arguments.
+
+    def add_signall(self, binding_set, keyval, modifiers, signal_name, binding_args=None):
+        """
+        Override or install a new key binding for keyval with modifiers on binding_set .
+
+        :param binding_set: a BindingSet to add a signal to
+        :param keyval: key value
+        :param modifiers: key modifier
+        :param signal_name: signal name to be bound
+        :param binding_args: list of BindingArg signal arguments.
+        :type binding_args: list()
+        """
+        if binding_args is None:
+            binding_args = list()
+
+        self.binding_set = binding_set
+        self.keyval = keyval
+        self.modifiers = modifiers
+        self.signal_name = signal_name
+        self.binding_args = binding_args
+
