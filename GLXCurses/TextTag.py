dadf490e9c6f10db68b59711e2a8fb17b2010676
Deitsuen <fireblack@orange.fr>
2017-04-09 15:21:52 +0200

    Add files via upload

---
+++ GLXCurses/TextTag.py
@@ -0,0 +1,16 @@
+class TextTag(object):
+
+    def text_tag_new(self, tag):
+        return tag
+
+    def text_tag_get_priority(self):
+        return self.text_tag_new
+
+
+
+
+
+if __name__ == '__main__':
+    text = TextTag()
+    print text.text_tag_get_priority()
+
