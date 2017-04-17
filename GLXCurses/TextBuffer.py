dfac933a188dac02c5f067430fb90a8967cc1c0d
Deitsuen <fireblack@orange.fr>
2017-04-17 16:57:16 +0200

    Update TextBuffer.py

--- GLXCurses/TextBuffer.py
+++ GLXCurses/TextBuffer.py
@@ -1,27 +1,33 @@
+
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# It script it publish under GNU GENERAL PUBLIC LICENSE
-# http://www.gnu.org/licenses/gpl-3.0.en.html
-# Author: Deitsuen
 import sys
 import uuid
 from GLXCurses import Application
 
-class TextBuffer(object):
+# It script it publish under GNU GENERAL PUBLIC LICENSE
+# http://www.gnu.org/licenses/gpl-3.0.en.html
+# Author: Deitsuen
 
+
+class TextBuffer(object):
     def __init__(self, table=None):
+
         """
         Create the new textBuffer object
+        ***Properties***
+        line_count - the number of
 
-        :param line_count - count the number of line and return the result
-        :param text - The contents of the buffer. Default value: "".
+
+        text - The contents of the buffer. Default value: "".
         """
 
         self.table = table
         self.tag_table = ""
-        self.text = """bonzour"""
+        self.text = 'lalalala'
+        self.buffer = ['Bonjour']
         self.slice = ""
-        
+
         # internal
         # Unique ID it permit to individually identify a widget by example for get_focus get_default
         self.id = uuid.uuid1().int
@@ -30,10 +36,13 @@ class TextBuffer(object):
         """
         Obtains the number of lines in the buffer.
         This value is cached, so the function is very fast.
+
+        :param line
         """
         return len(self.text.split('\n'))
 
     def get_char_count(self):
+
         """
         Gets the number of characters in the buffer;
         :Note:
@@ -41,46 +50,38 @@ class TextBuffer(object):
         you can’t e.g. expect the contents of the buffer in string form to be this many bytes long.
         The character count is cached, so this function is very fast.
         """
-        return len(self.text)
+        return len(self.buffer)
 
-    def get_tag_table(self):
+    def text_buffer_get_tag_table(self):
         """
         Get the :class:`TextTagTable <GLXCurses.TextTagTable.TextTagTable>` associated with this buffer.
         """
-        if None:
-            return None
+        pass
 
-        def insert(self, text):
-        len_bytes = sys.getsizeof(text)
-        iter = list(self.text)
-
-        if len_bytes <= -1:
+    def insert(self):
+        text = 'Ceci est un texte'
+        lenght_of_byte = sys.getsizeof(text)
+        if lenght_of_byte == -1:
             text = None
-            iter.insert(0, text)
-            self.emit_insert()
-            if iter != list(self.text):
-                iter.remove(None)
-                iter.insert(-1, text)
-                return iter
+            self.buffer.insert(1, text)
+            self.emit_insert_text()
         else:
-            text = text
-            return str(text)
+            self.buffer.insert(1,text)
 
-    def emit_insert(self):
+        return self.buffer
+
+    def emit_insert_text(self):
         instance = {
             'class': self.__class__.__name__,
             'type': 'insert-text',
             'id': self.id
         }
-        # event emit
-        Application.emit('SIGNALS', instance)
+        # EVENT EMIT
+        Application().emit('SIGNALS', instance)
+
 
 if __name__ == '__main__':
     textbuffer = TextBuffer()
-    print ("Text:      :"+str(textbuffer.get_char_count()))
-    print ("Text:      :"+str(textbuffer.get_line_count()))
-    print textbuffer.insert("This is a test")
-
-
-
-    # W.I.P (Work In Progress)
+    print ("Text:      :" + str(textbuffer.get_char_count()))
+    print ("Text:      :" + str(textbuffer.get_line_count()))
+    print textbuffer.insert()
