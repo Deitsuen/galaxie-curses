5ad0f1793c774c61fda99434893315e8ace45f6a
Tuuux <tuxa@rtnp.org>
2017-07-02 10:14:04 +0200

    Misc 100% tested and documented

--- GLXCurses/Misc.py
+++ GLXCurses/Misc.py
@@ -33,13 +33,21 @@ class Misc(Widget):
         .. py:attribute:: xpad   - The amount of space to add on the left and right of the widget, in characters
         .. py:attribute:: ypad   - The amount of space to add above and below the widget, in characters
         """
+        # Load heritage
         Widget.__init__(self)
+
+        # It's a GLXCurse Type
+        self.glxc_type = 'GLXCurses.Misc'
+
+        # Widgets can be named, which allows you to refer to them from a GLXCStyle
+        self.set_name('Misc')
+
         self.xalign = 0.0
         self.xpad = 0
         self.yalign = 0.0
         self.ypad = 0
 
-    def set_alignment(self, xalign, yalign):
+    def set_alignment(self, xalign=None, yalign=None):
         """
         Sets the alignment of the widget.
 
@@ -48,22 +56,32 @@ class Misc(Widget):
         :type xalign: :py:obj:`float`
         :type yalign: :py:obj:`float`
         """
-        # xalign :
+        # Force a default value
+        if xalign is None:
+            xalign = 0.0
+        if yalign is None:
+            yalign = 0.0
+
+        # clamp xalign :
         xalign = float(xalign)
         if xalign >= 1.0:
             xalign = 1.0
         elif xalign < 0.0:
             xalign = 0.0
 
-        # yalign :
+        # clamp yalign :
         yalign = float(yalign)
         if yalign >= 1.0:
             yalign = 1.0
         elif yalign < 0.0:
             yalign = 0.0
 
-        self.xalign = xalign
-        self.yalign = yalign
+        # Update value if needed
+        if self.xalign != xalign:
+            self.xalign = xalign
+
+        if self.yalign != yalign:
+            self.yalign = yalign
 
     def get_alignment(self):
         """
@@ -75,21 +93,30 @@ class Misc(Widget):
         .. py:attribute:: yalign - location to store Y alignment of misc , or NULL.
 
         :return: xalign, yalign
-        :rtype: float, float
+        :rtype: tuple
         """
         return self.xalign, self.yalign
 
-    def set_padding(self, xpad, ypad):
+    def set_padding(self, xpad=None, ypad=None):
         """
         Sets the amount of space to add around the widget.
 
+        None can be use for set padding to 0
+
         :param xpad: the amount of space to add on the left and right of the widget, in char.
         :param ypad: the amount of space to add on the top and bottom of the widget, in char.
-        :type xpad: Float
-        :type ypad: Float
+        :type xpad: int or None
+        :type ypad: int or None
         """
-        self.xpad = int(xpad)
-        self.ypad = int(ypad)
+        if xpad is None:
+            xpad = 0
+        if ypad is None:
+            ypad = 0
+        if type(xpad) == int and type(ypad) == int:
+            self.xpad = xpad
+            self.ypad = ypad
+        else:
+            raise TypeError(u'>xpad< and >ypad< must be int type or None')
 
     def get_padding(self):
         """
@@ -101,5 +128,6 @@ class Misc(Widget):
         .. py:attribute:: ypad - location to store padding in the Y direction, or NULL.
 
         :return: xpad, ypad
-        :rtype: float, float        """
+        :rtype: tuple
+        """
         return self.xpad, self.ypad
