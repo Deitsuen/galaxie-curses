5d5eb1b26d4695226a15ca3a487a28de40a3291b
Deitsuen <fireblack@orange.fr>
2017-08-31 21:32:05 +0200

    add test_insert_range and test_insert_range_interactive

--- test/test_TextBuffer.py
+++ test/test_TextBuffer.py
@@ -5,7 +5,7 @@ from GLXCurses.Utils import glxc_type
 # Unitest
 
 position = 0
-textbuffer = TextBuffer.TextBuffer()
+textbuffer = TextBuffer()
 
 
 class TestTextBuffer(unittest.TestCase):
@@ -20,8 +20,8 @@ class TestTextBuffer(unittest.TestCase):
         self.assertEqual(textbuffer.get_char_count(), 36)
 
     def test_insert(self):
-        textbuffer_insert = TextBuffer.TextBuffer().insert
-        self.assertEqual(textbuffer_insert(position, 'test')[position], 'test')
+        self.assertEqual(textbuffer.insert(position, 'test').index('test'), position)
+        self.assertEqual(textbuffer.insert(position, 'test')[position], 'test')
 
     def test_insert_at_cursor(self):
         textbuffer.insert(position, 'Ceci est un texte')
@@ -48,6 +48,45 @@ class TestTextBuffer(unittest.TestCase):
         self.assertTrue(textbuffer.insert_interactive(4, 'toto'))
         self.assertTrue(textbuffer.insert_interactive(5, 'toto'))
         self.assertTrue(textbuffer.insert_interactive(6, 'toto'))
+
+    def test_insert_range(self):
+
+        # text = textbuffer.table[0]
+        # tag = textbuffer.table[1]
+        text = 0
+        tag = 1
+        buffer_test = []
+
+        print textbuffer.insert_range(buffer_test, text, tag)[0]
+
+        self.assertEqual(textbuffer.insert_range(buffer_test, text, tag)[0], textbuffer.table[0])
+        self.assertIn(textbuffer.insert_range(buffer_test, text, tag)[0], textbuffer.table)
+
+        self.assertEqual(textbuffer.insert_range(buffer_test, text, tag)[1], textbuffer.table[1])
+        self.assertIn(textbuffer.insert_range(buffer_test, text, tag)[1], textbuffer.table)
+
+    def test_insert_range_interactive(self):
+        textbuffer.view_edit()
+        buffer_test = []
+
+        # 0 no editable
+        # 1 no editable
+        # 2 no editable
+        # 3 no editable
+        # 4 editable
+        # 5 editable
+        # 6 editable
+
+        # No Editable
+        self.assertFalse(textbuffer.insert_range_interactive(buffer_test, 0, 0, 1))
+        self.assertFalse(textbuffer.insert_range_interactive(buffer_test, 1, 0, 1))
+        self.assertFalse(textbuffer.insert_range_interactive(buffer_test, 2, 0, 1))
+        self.assertFalse(textbuffer.insert_range_interactive(buffer_test, 3, 0, 1))
+
+        # Editable
+        self.assertTrue(textbuffer.insert_range_interactive(buffer_test, 4, 0, 1))
+        self.assertTrue(textbuffer.insert_range_interactive(buffer_test, 5, 0, 1))
+        self.assertTrue(textbuffer.insert_range_interactive(buffer_test, 6, 0, 1))
 
 
 if __name__ == '__main__':
