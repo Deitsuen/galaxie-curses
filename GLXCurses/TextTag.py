20eab4026470fa90594e66995daaf3ec80af9ff5
Deitsuen <fireblack@orange.fr>
2017-08-31 21:34:06 +0200

    small reworks

--- GLXCurses/TextTag.py
+++ GLXCurses/TextTag.py
@@ -23,18 +23,11 @@ class TextTag(object):
         self.buffer = ['toto', 'tata', 'tutu', 'titi']
 
     def text_tag(self, text,  start, end, attribute, attribute2='off', attribute3='off'):
-        print self.color[attribute] + self.color[attribute2] + self.color[attribute3] + str(text[start:end])
+        return self.color[attribute] + self.color[attribute2] + self.color[attribute3] + str(text[start:end])
 
-    def tag_bold(self):
-        print self.text_tag(self.buffer, 0, 4, 'bold')
-
-    def tag_underline(self):
-        print self.text_tag(self.buffer, 0, 4, 'underline')
 
 if __name__ == '__main__':
     tag = TextTag()
 
     print tag.text_tag(tag.buffer, 0, 1, 'bold')
-    print tag.tag_bold()
-    print tag.tag_underline()
 
